4f8d36ee4e231282ad3c65d64e5d2de8b14b4e44
9Diagen <9diagen@gmail.com>
2023-11-17 15:44:40 +0500

    15:17 commit

--- src/main/java/diag/playmine/com/Events/OnJoin.java
+++ src/main/java/diag/playmine/com/Events/OnJoin.java
@@ -1,7 +1,11 @@
 package diag.playmine.com.Events;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Objects;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import diag.playmine.com.AScoreBoard;
 import diag.playmine.com.PlayerScoreBoard.ScoreBoard;
 import org.bukkit.Bukkit;
@@ -16,12 +20,53 @@ public class OnJoin implements Listener {
     public OnJoin() {
     }
 
+    public static class PlayerInfo {
+        public String playerName;
+        public String balance;
+        public String currentExp = "0";
+        public String playerLevel = "0";
+
+        public String id;
+
+        public void setBalance(String balance) {
+            this.balance = balance;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public void setPlayerName(String playerName) {
+            this.playerName = playerName;
+        }
+    }
+
+
     @EventHandler
     public void onJoin(PlayerJoinEvent e) throws JSONException, IOException {
         Player p = e.getPlayer();
+        String path = ScoreBoard.getPath(AScoreBoard.getPlugin(), p.getUniqueId() + "");
+        File f = new File(path);
+        if (!f.exists()){
+            PlayerInfo playerInfo = new PlayerInfo();
+            writeData(p, path, playerInfo);
+        }
         new ScoreBoard.sendActionBarMessage(p);
         Plugin plugin = Bukkit.getPluginManager().getPlugin("aScoreBoard");
         assert plugin != null;
-        p.setScoreboard(ScoreBoard.newScoreBoard(ScoreBoard.getPath(plugin, String.valueOf(p.getUniqueId()))));
+        p.setScoreboard(ScoreBoard.newScoreBoard(path));
+    }
+
+    public void writeData(Player player, String path, PlayerInfo playerInfo) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        playerInfo.setBalance(AScoreBoard.getPlugin().getConfig().getString("startbalance"));
+        playerInfo.setPlayerName(Objects.requireNonNull(player.getPlayer()).getName());
+        playerInfo.setId(player.getUniqueId().toString());
+        String result = objectMapper.writeValueAsString(playerInfo);
+        File file = new File(path);
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write(result);
+        fileWriter.flush();
+        fileWriter.close();
     }
 }
